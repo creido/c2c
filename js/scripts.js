410c4d1427c89ca73bf2cbd31044e1fa77cd36cd
Chris Reid <chris.reid@akqa.com>
2017-07-31 14:34:56 +0100

    Switched API key, added noindex meta

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 const ZIP_CODE = '71923';
-const key = 'AIzaSyAvdOyCtU22baeA6Yc3kFVxqehIYrIBc4w';
-// const key = 'AIzaSyCQmU30AQBeUepZcXktk3W4qawZdIMOtm8';
+// const key = 'AIzaSyAvdOyCtU22baeA6Yc3kFVxqehIYrIBc4w';
+const key = 'AIzaSyCQmU30AQBeUepZcXktk3W4qawZdIMOtm8';
 const url = `https://content.googleapis.com/civicinfo/v2/representatives?address=${ZIP_CODE}&includeOffices=true&levels=locality&roles=legislatorLowerBody&alt=json&key=${key}`;
 
 const success = (data) => {
